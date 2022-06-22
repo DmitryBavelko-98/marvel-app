d2f99157d7795f449527a3fd1d91d6842da7ddd8
DmitryBavelko-98 <bavelko90@gmail.com>
2022-06-22 17:01:03 +0300

    Fix default image

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 
 import './randomChar.scss';
-import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -52,6 +51,8 @@ class RandomChar extends Component {
         const spinner = loading ? <Spinner/> : null;
         const content = !(loading || error) ? <View char={char}/> : null; 
 
+        console.log()
+
         return (
             <div className="randomchar">
                 {errorMessage}
@@ -68,7 +69,6 @@ class RandomChar extends Component {
                     <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
-                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
                 </div>
             </div>
         )
@@ -78,9 +78,15 @@ class RandomChar extends Component {
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
 
+    let styles = {};
+
+    if (thumbnail.includes('not_available')) {
+        styles = {objectFit: 'cover'}
+    }
+
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} style={styles} alt="Random character" className="randomchar__img"/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
