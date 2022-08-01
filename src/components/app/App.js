dacc5f67cbe0d84374195bb6ae4e5789e052683a
DmitryBavelko-98 <bavelko90@gmail.com>
2022-08-01 11:25:31 +0200

    Add lazy imports

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,29 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
 
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
 
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+
+// 670
 const App = () => {
     return (
         <Router>
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path='/marvel-app' element={<MainPage/>}/>
-                        <Route path='/marvel-app/comics' element={<ComicsPage/>}/>
-                        <Route path='/marvel-app/comics/:comicId' element={<SingleComicPage/>}/>
-                        <Route path='*' element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path='/marvel-app' element={<MainPage/>}/>
+                            <Route path='/marvel-app/comics' element={<ComicsPage/>}/>
+                            <Route path='/marvel-app/comics/:comicId' element={<SingleComicPage/>}/>
+                            <Route path='*' element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import Spinner from "../spinner/Spinner";
 
 import decoration from '../../resources/img/vision.png';
+
+const CharInfo = lazy(() => import('../charInfo/CharInfo'));
 
 const MainPage = () => {
     const [selectedChar, setChar] = useState(null);
@@ -23,9 +25,11 @@ const MainPage = () => {
                 <ErrorBoundary>
                     <CharList onCharSelected={onCharSelected}/>
                 </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-                </ErrorBoundary>
+                <Suspense fallback={<Spinner/>}>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                </Suspense>
             </div>
             <img className="bg-decoration" src={decoration} alt="vision"/>
         </>
